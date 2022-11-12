55082b9d81b7998ca55c4525334f6a533c54e7df
Ralin Chimev <ralin.chimev@gmail.com>
2022-11-12 08:44:36 +0200

    Create service object

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,32 +8,35 @@ import {
   Body,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
-  findAll(@Query() query) {
-    const { offset, limit } = query;
-    return `coffees: pagination ${offset} ${limit}`;
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `coffee ${id}`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `update ${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `delete ${id}`;
+    return this.coffeesService.remove(id);
   }
 }
