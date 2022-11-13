4b9a1c74b7e10c2bfa0a6106635139c0045bccc7
Ralin Chimev <ralin.chimev@gmail.com>
2022-11-13 12:27:15 +0200

    Add pagination

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
       transform: true,
+      transformOptions: { enableImplicitConversion: true },
       whitelist: true,
     }),
   );
+
   await app.listen(3000);
 }
+
 bootstrap();
